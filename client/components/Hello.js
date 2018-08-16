c5820f999933cb3bebadea3b227f024211d8aa54
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-08-16 19:51:16 +0200

    Init project

---
+++ client/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+(props, context) => {
+  return ();
+}
+
+Hello.propTypes = {
+};
+
+export default Hello;
---
+++ client/modules/App/components/Hello2/Hello2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+(props, context) => {
+  return ();
+}
+
+Hello2.propTypes = {
+};
+
+export default Hello2;
